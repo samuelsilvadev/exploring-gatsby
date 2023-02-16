3b5f08f229a7a2fec57a8f0c4cff48781f6e6b44
Samuel Silva <samuelsilvawb@gmail.com>
2023-02-16 22:17:47 +0000

    feat: setup gatsby plugins to enable posts to be automatically created as pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,28 @@ module.exports = {
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     image: 'https://www.yourdomain.tld/images/og-image.jpg',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          posts: require.resolve('./src/components/Layout.tsx'),
+        },
+      },
+    },
+  ],
 };
